79dbe38a8de476b4b37ff54cecc32d9b62c21bc7
MaximusIII <bernhard.roehrl@googlemail.com>
2022-12-01 21:58:02 +0100

    Init actual Project after testing

--- RL Testing.py
+++
@@ -1,6 +0,0 @@
-# so ich mach dann mal paar änderungen
-#und schau mir mal an wie sich das ganze ausgeht
-#eigt will ich das dann mal ne rihtige abzweigung zu sehen ist
-
-#noch ein 2 tests
-#34 tests
