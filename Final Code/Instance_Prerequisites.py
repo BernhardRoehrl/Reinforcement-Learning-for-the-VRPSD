2190dbba0f91cfc27b60c97bd76c91c41644a0f3
MaximusIII <bernhard.roehrl@googlemail.com>
2023-06-27 00:23:35 +0200

    Class file handling Instances and their Generation and associated data

--- Final Code/Instance_Prerequisites.py
+++ Final Code/Instance_Prerequisites.py
@@ -1,50 +1,50 @@
 import numpy as np
-import json
 import os
-
-"""Profile"""
-solomon_id = 0  # Contains Unique ID of Instance and C/R in the beginning for Clustered or Random Customer Spread
-capacity = 100  # Maximum Capacity H of the Vehicle
-demand_bottom = 10  # Minimum possible customer demands
-demand_top = 70  # Maximum possible customer demands
-demand_arr = [demand_top, demand_bottom]  # Array of Bounds
-demand_mean = np.mean(demand_arr)  # Mean of Demands for Simple Policy
 
 
 class Instance:
-
+    # Initialize the instance with given parameters
     def __init__(self, filename, size, capacity, demand_bottom, demand_top):
-        self.filedir = r'D:\Code\VRP OR\Solomon Instances'
+        self.filedir = r'D:\Code\VRP OR\Solomon Instances'  # Define directory where instances are stored
         self.filename = os.path.join(self.filedir, filename if filename.endswith('.txt') else filename + ".txt")
-        self.size = size
+        # Join the directory and filename to get full file path
+        self.size = size  # The size of the instance
+        if size <= 20:  # Dynamic Sizing Category
+            self.instance_size_category = "small"
+        elif 30 < size <= 60:
+            self.instance_size_category = "medium"
+        else:
+            self.instance_size_category = "large"
         self.demand_top = demand_top
         self.demand_bottom = demand_bottom
         self.capacity = capacity
         self.demand_mean = np.mean([self.demand_top, self.demand_bottom])
         self.solomon_id, self.customers = self.parse_file()
         self.distance_matrix, self.customers = self.create_distance_matrix()
-        self.capacity_check()
-        self.profile = {
+        self.capacity_check()  # make sure no illegal moves are done
+        self.profile = {  # save the profile for instance naming
             "customer_spread": self.solomon_id[0],
             "solomon_id": self.solomon_id[1:],
-            "N": int(len((self.customers)))-1,
+            "N": int(len((self.customers))) - 1,
             "capacity": self.capacity,
             "Demand_Top": self.demand_top,
             "Demand_Bottom": self.demand_bottom,
-            "%": self.capacity/self.demand_mean,
+            "%": self.capacity / self.demand_mean,
+            "InstanceSize": self.instance_size_category
         }
-        self.instance_name = f"{self.profile['customer_spread']}{self.profile['solomon_id']}_N{self.profile['N']}_H{self.profile['capacity']}_D{self.profile['Demand_Bottom']}-{self.profile['Demand_Top']}_%_{round(self.profile['%'], 2)}"
-
+        self.instance_name = f"{self.profile['customer_spread']}{self.profile['solomon_id']}_N{self.profile['N']}_H{self.profile['capacity']}_D{self.profile['Demand_Bottom']}-{self.profile['Demand_Top']}_%{round(self.profile['%'], 2)}"
+        # One Way Print out of the whole important configuration
 
     def parse_file(self):
+        """Read in the Solomon File's Information and keep the relvant"""
         with open(self.filename, 'r') as file:
             lines = file.readlines()
             solomon_id = lines[0].strip()  # the instance id is always in the first line
             customer_lines = lines[8:]  # remaining lines with relevant information
-            customers = []
+            customers = []  # init an empty list
             for line in customer_lines:
                 details = line.strip().split()
-                if len(details) >= 3:
+                if len(details) >= 3:  # create a dic with the data we extract
                     customer = {
                         "id": int(details[0]),
                         "x": int(details[1]),
@@ -54,20 +54,23 @@ class Instance:
         return solomon_id, customers
 
     def capacity_check(self):
+        """Check for Illegal Instance Configuration"""
         if self.capacity < self.demand_top:
             raise Exception("!!!ILLEGAL PROFILE CAPCITY HAS TO BE EQUAL OR BIGGER THAN DEMAND TOP!!!")
 
         if self.demand_bottom > self.demand_top:
             raise Exception("!!!ILLEGAL PROFILE Demand Bottom HAS TO BE EQUAL OR smaller THAN DEMAND TOP!!!")
 
-
     def euclidean_distance(self, x1, y1, x2, y2):
         """calculate distances"""
         return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
 
     def create_distance_matrix(self):
+        """Cut the extracted data to desired size and calculate distance matrix"""
+        """Cutting"""
         self.customers = [customer for customer in self.customers if customer['id'] <= self.size]
 
+        """Distance Matrix"""
         n = len(self.customers)
         distance_matrix = np.zeros((n, n))  # init distance matrix with zeros
         for i in range(n):  # Go through data
@@ -81,6 +84,8 @@ class Instance:
         return distance_matrix, self.customers
 
 
+"""Testing Purposes"""
+
 if __name__ == "__main__":
     solomon_flex = Instance('C101', size=5, capacity=19, demand_bottom=20, demand_top=19)
     print("Customers: ", solomon_flex.customers)
@@ -89,5 +94,3 @@ if __name__ == "__main__":
     print(len(solomon_flex.customers))
     print(solomon_flex.distance_matrix)
     print(solomon_flex.instance_name)
-
-
